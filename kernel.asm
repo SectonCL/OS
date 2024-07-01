586c77828c7b558df4e2353c97d757702a0eb6d2
Sectonidse <94748461+Sectonidse@users.noreply.github.com>
2024-07-01 13:02:52 +0300

    Update kernel.asm

--- kernel.asm
+++ kernel.asm
@@ -1,20 +1,15 @@
-;; kernel.asm
-;; version 0.0.1
-
-bits 32		;nasm directive
+bits 64        ; nasm directive
 section .text
-	;multiboot spec
-	align 4
-	dd 0x1BADB002			;magic
-	dd 0x00				;flags
-	dd - (0x1BADB002 + 0x00)	;checksum. m+f+c should be zero
+    ; multiboot spec
+    align 8
+    dd 0x1BADB002            ; magic
+    dd 0x00                  ; flags
+    dd - (0x1BADB002 + 0x00) ; checksum. m+f+c should be zero
 
 global start
-extern k_main	;k_main is defined in the kernel.c file
+extern k_main  ; k_main is defined in the kernel.c file
 
 start:
-	cli  ; stop interrupts
-
-	call k_main
-
-	hlt ; halt the CPU
+    cli  ; stop interrupts
+    call k_main
+    hlt  ; halt the CPU
